ca3710788f236656787422ec89008f97114a6a1e
cammac <camm@rocktechcomputer.com>
2019-04-19 09:31:31 -0700

    array is rendering

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,25 +1,20 @@
 import axios from 'axios';
 
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
-
 export const FETCH_SMURF_START = 'FETCH_SMURF_START';
 export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS';
 export const FETCH_SMURF_FAIL = 'FETCH_SMURF_FAIL';
-export const ADD_SMURF_START = 'ADD_SMURF_START';
-export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
-export const ADD_SMURF_FAIL = 'ADD_SMURF_FAIL';
+// export const ADD_SMURF_START = 'ADD_SMURF_START';
+// export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+// export const ADD_SMURF_FAIL = 'ADD_SMURF_FAIL';
 
 export const getSmurf = () => dispatch => {
-  // console.log('fetch')
+  console.log('fetching')
   dispatch({ type: FETCH_SMURF_START });
 
-  axios
-    .get('http://localhost:3333/smurfs')
+  return axios
+    .get("http://localhost:3333/smurfs")
     .then(res => {
-      // console.log('get: ', res.data)
+      console.log('get: ', res)
       dispatch({
         type: FETCH_SMURF_SUCCESS,
         payload: res.data
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -5,13 +5,27 @@ import { getSmurf } from '../actions';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getSmurf();
+  }
+
   render() {
+    console.log('smurfs: ', this.props.smurfs)
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <div>
+          {this.props.fetchingSmurf ? <h2>Loading...</h2> : null}
+          {this.props.smurfs.map(smurf =>
+            <div key={smurf.id}>
+              <h3>{smurf.name}</h3>
+              <p>{smurf.age}</p>
+              <p>{smurf.height}</p>
+            </div>
+          )}
+        </div>
+
       </div>
     );
   }
@@ -19,6 +33,7 @@ class App extends Component {
 
 const mapStateToProps = state => {
   return {
+    smurfs: state.smurfs,
     fetchingSmurf: state.fetchingSmurf,
     error: state.error
   }
